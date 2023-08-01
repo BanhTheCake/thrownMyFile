7a58e55035bba352492229a07f3036932dc49ac2
Binhanh <binhanh080103@gmail.com>
2023-08-01 10:47:18 +0700

    Add configModule for server and move port to file.env

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,9 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EventsModule } from './events/events.module';
-
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [EventsModule],
+  imports: [EventsModule, ConfigModule.forRoot({ isGlobal: true })],
   controllers: [AppController],
   providers: [AppService],
 })
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configServices = app.get(ConfigService);
   app.enableCors({
     origin: '*',
   });
-  await app.listen(3001);
+  await app.listen(configServices.get('PORT') || 3000);
 }
 bootstrap();
